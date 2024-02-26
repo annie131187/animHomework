824b5895e17ad4fd3ce32c876ea11ced339ec540
AppVeyor <ci@appveyor.com>
2024-02-26 05:31:12 +0000

    aeb0392

---
+++ main.js
@@ -0,0 +1,20 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+
+;// CONCATENATED MODULE: ./src/js/app.js
+
+const app_button = document.querySelector(".button");
+const collapse = document.querySelector(".collapse");
+app_button.addEventListener("click", evt => {
+  evt.preventDefault();
+  collapse.classList.toggle("hide");
+});
+;// CONCATENATED MODULE: ./src/index.js
+
+
+
+// TODO: write your code in app.js
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
